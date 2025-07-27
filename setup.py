e64d8784e2b9dcd2c9ce8baeeac8ad76835f0176
asafusa553 <investingdotcom3@gmail.com>
2025-07-27 13:17:11 +0300

    fixed name

--- setup.py
+++ setup.py
@@ -3,19 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='conttts',
     version='1.0.0',
-    author='Your Name',
-    description='A GUI + CLI for ContTTS',
+    author='Assaf Almog',
+    description='ContTTS: A GUI and CLI for TTS',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'conttts=conttts.cli:main',
         ],
     },
-    install_requires=[
-        # Add your dependencies here if any
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
+    install_requires=[],
+    python_requires='>=3.6',
 )
