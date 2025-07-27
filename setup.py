fc48571d2d523ec4cedf827c034b5ac2bae8f78a
asafusa553 <investingdotcom3@gmail.com>
2025-07-27 13:40:34 +0300

    Added package installs

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
-    name='conttts',
-    version='1.0.0',
-    author='Assaf Almog',
-    description='ContTTS: A GUI and CLI for TTS',
+    name="conttts",
+    version="1.0.0",
+    author="Assaf Almog",
+    description="ContTTS Studio - An advanced Coqui TTS GUI application",
+    long_description=open("README.md", encoding="utf-8").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
+    url="https://github.com/AssafAlmogDev/Cont-TTS-Studio",
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "sounddevice",
+        "soundfile",
+        "pydub",
+        "num2words",
+        "TTS",  # Coqui TTS
+        "pillow",  # for image handling if used
+    ],
     entry_points={
-        'console_scripts': [
-            'conttts=conttts.cli:main',
+        "console_scripts": [
+            "conttts=conttts.cli:main",
         ],
     },
-    install_requires=[],
-    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
